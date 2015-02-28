38eb5059a0cce9726b6235be31fc7e363cf50c98
Kimchelly <kmtao88@gmail.com>
2015-02-28 12:22:26 -0500

    Added some code for finding all article post times, like and dislike counts

--- test.py
+++ test.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
+
 import requests
-
-url = raw_input("Enter the Bwog article URL: ")
-
-r = requests.get("http://" + url)
-
+# Assuming all articles are posted on the front page of Bwog, gets all the
+# dates of the articles (and all the urls associated with the dates).
+r = requests.get("http://bwog.com")
 data = r.text
 
 soup = BeautifulSoup(data)
-all_comments = soup.findAll("div", "reg-comment-body")
+article_dates = soup.findAll(class_="post-datetime")
+print(article_dates)
 
-print all_comments
 
+# Gets information for a specific article
+url = input("Enter the Bwog article URL: ")
+r = requests.get("http://" + url)
+
+# Gets all comments, likes, and dislikes (in sequential order but not parsed
+# out yet)
+all_comments = soup.findAll(class_="reg-comment-body")
+all_likes = soup.findAll(class_="like-count")
+all_dislikes = soup.findAll(class_="dislike-count")
+
+print(all_comments)
+print(all_likes)
+print(all_dislikes)
