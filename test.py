b119fc19d7a15a1d56bfa44e975dd73e32601912
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-06 13:58:02 -0500

    Add time module

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
+import requests, time
 
-import requests
 # Assuming all articles are posted on the front page of Bwog, gets all the
 # dates of the articles (and all the urls associated with the dates).
 r = requests.get("http://bwog.com")
 data = r.text
+today = time.strftime("%d/%m/%Y")
 
 soup = BeautifulSoup(data)
 article_dates = soup.findAll(class_="post-datetime")
 print(article_dates)
-
 
 # Gets information for a specific article
 url = input("Enter the Bwog article URL: ")
