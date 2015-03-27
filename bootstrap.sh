e3c607f15e9f282cfad13bbf14854bd99a0ca6da
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-27 16:16:26 -0400

    Add database model and reorganize

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y python
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip
+sudo apt-get install -y vim
+sudo pip install flask
+sudo pip install flask-sqlalchemy
+sudo pip install sqlalchemy-migrate
+sudo pip install jinja2
+sudo pip install werkzeug
+
+echo hello
