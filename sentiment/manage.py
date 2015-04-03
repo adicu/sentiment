150d5711ee752e3dca90dd4cd143bd1c83462a28
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-03 14:38:21 -0400

    Add function to create database

--- sentiment/manage.py
+++ sentiment/manage.py
@@ -10,5 +10,20 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+from migrate.versioning import api
+from config.config import SQLALCHEMY_DATABASE_URI
+from config.config import SQLALCHEMY_MIGRATE_REPO
+import os.path
+
+@manager.command
+def create_db():
+    db.create_all()
+
+    if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+        api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+    else:
+        api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+
 if __name__ == '__main__':
     manager.run()
