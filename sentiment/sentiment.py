e1f8764d13df3e9f2fa71767f175b7dc65078de0
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-10 14:00:58 -0400

    Move database to script

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -4,13 +4,7 @@ from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
 app.config.from_object('config')
-db = SQLAlchemy(app)
-
-
-import models
 
 
 @app.route("/")
