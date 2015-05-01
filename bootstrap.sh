ab6a9eba1e2466d10abbb27d9b5878aae0a29aed
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-01 19:30:27 -0400

    Add smooth scrolling

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,10 @@ sudo apt-get install -y vim
 sudo pip install flask
 sudo pip install flask-sqlalchemy
 sudo pip install flask-migrate
-sudo pip install bs4
-sudo pip install jinja2
+sudo pip install beautifulsoup4
 sudo pip install textblob
+sudo pip install requests
+sudo pip install jinja2
 sudo pip install werkzeug
 
 echo hello
